cc7734ee9ce53088e8b76cb04a162e206146b8e4
Akira Sudoh <asudoh@gmail.com>
2020-10-12 18:28:07 +0900

    Service integration

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-menu-button.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-logo.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/top-nav.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/top-nav-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/top-nav-menu.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/top-nav-menu-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/left-nav.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/left-nav-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/left-nav-menu.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/left-nav-menu-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/left-nav-overlay.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-global-bar.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-profile.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-profile-item.js';
-import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-search-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/masthead/masthead-container.js';
+import '@carbon/ibmdotcom-web-components/es/components/footer/footer-container.js';
 import './index.scss';
 
 window.digitalData = {
